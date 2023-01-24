1e1e86830ae2b4222d0be794e00658b55fbea054
Yago Lopez <yago.lopez@gmail.com>
2023-01-24 22:15:31 +0100

    Add cached images

--- frontend/site/next.config.js
+++ frontend/site/next.config.js
@@ -47,3 +47,4 @@ module.exports = withCommerceConfig({
 
 // Don't delete this console log, useful to see the commerce config in Vercel deployments
 console.log('next.config.js', JSON.stringify(module.exports, null, 2))
+console.log('process.env.NODE_ENV:', process.env.NODE_ENV)
