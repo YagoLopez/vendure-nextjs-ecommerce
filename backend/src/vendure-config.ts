138db2a2ff8a227ffcfee7288bb06582900ca7b2
Yago Lopez <yago.lopez@gmail.com>
2023-01-26 11:53:06 +0100

    Add production run script BE and FE

--- backend/src/vendure-config.ts
+++ backend/src/vendure-config.ts
@@ -75,11 +75,11 @@ export const config: VendureConfig = {
             templatePath: path.join(__dirname, '../static/email/templates'),
             globalTemplateVars: {
                 // The following variables will change depending on your storefront implementation.
-                // Here we are assuming a storefront running at http://localhost:8080.
+                // Here we are assuming a storefront running at http://localhost:8000.
                 fromAddress: '"example" <noreply@example.com>',
-                verifyEmailAddressUrl: 'http://localhost:8080/verify',
-                passwordResetUrl: 'http://localhost:8080/password-reset',
-                changeEmailAddressUrl: 'http://localhost:8080/verify-email-address-change'
+                verifyEmailAddressUrl: 'http://localhost:8000/verify',
+                passwordResetUrl: 'http://localhost:8000/password-reset',
+                changeEmailAddressUrl: 'http://localhost:8000/verify-email-address-change'
             },
         }),
         AdminUiPlugin.init({
